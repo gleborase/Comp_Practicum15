b23a978fe9982a49daa760dcf918d9d05e17a43e
gleborase <krasnopolsci@gmail.com>
2024-12-25 10:04:08 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{/// <summary>
+/// Задача 3. Вывести четные двузначные числа и подсчитать их количество.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int count = 0;
+            for (int x = 10; x <= 99; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(x);
+                    count++;
+                }
+            }
+            Console.WriteLine($"count={count}");
+        }
+
+    }
+}
