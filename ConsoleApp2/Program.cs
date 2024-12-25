1c87ea51d88fab9015c1325f23aaa0fdd0574d40
gleborase <krasnopolsci@gmail.com>
2024-12-25 10:13:36 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{/// <summary>
+/// Задача 4. Генерируются 10 случайных целых чисел в интервале (-40, 40). Вывести эти числа.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            Random random = new Random();
+            for (int x = 0; x < 10; x++)
+            { 
+            int number=random.Next(-40,40);
+                Console.WriteLine(number);  
+            }
+        }
+    }
+}
